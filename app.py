540f630a0b610f943cbbbc92f43396cdb69e44b9
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-05-02 14:45:18 -0300

    Foi corrigido o codigo no app.py (alteração de Faculdade para InstituicaodeEnsino)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = 'minha-chave-teste'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Faculdade(db.Model):
+class InstituicaodeEnsino(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,24 +16,24 @@ class Faculdade(db.Model):
     instituicao = db.Column(db.String(100), nullable=True)
     
     def __repr__(self):
-        return f'<Faculdade {self.nome}>'
+        return f'<InstituicaodeEnsino {self.nome}>'
 
 with app.app_context():
     db.create_all()
     try:
-        if not Faculdade.query.filter_by(email='teste@email.com').first():
-            faculdade = Faculdade(
+        if not InstituicaodeEnsino.query.filter_by(email='teste@email.com').first():
+            instituicaodeEnsino = InstituicaodeEnsino(
                 nome='Fernando',
                 email='teste@email.com',
                 senha=generate_password_hash('123456'),
                 instituicao='Uninassau'
             )
-            db.session.add(faculdade)
+            db.session.add(instituicaodeEnsino)
             db.session.commit()
-            print('Faculdade mockado criado!')
+            print('Instituição de Ensino mockado criado!')
     except IntegrityError:
         db.session.rollback()
-        print('Faculdade já existe.')
+        print('Instituição de Ensino já existe.')
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
@@ -44,13 +44,13 @@ def cadastro():
         instituicao = request.form['instituicao']
 
         try:
-            nova_faculdade = Faculdade(
+            nova_instituicaodeEnsino = InstituicaodeEnsino(
                 nome=nome,
                 email=email,
                 senha=generate_password_hash(senha),
                 instituicao=instituicao
             )
-            db.session.add(nova_faculdade)
+            db.session.add(nova_instituicaodeEnsino)
             db.session.commit()
             flash('Cadastro realizado com sucesso! Faça login agora.')
             return redirect(url_for('login'))
@@ -70,9 +70,9 @@ def login():
         email = request.form['email']
         senha = request.form['senha']
 
-        faculdade = Faculdade.query.filter_by(email=email).first()
-        if faculdade and check_password_hash(faculdade.senha, senha):
-            session['faculdade_id'] = faculdade.id
+        instituicaodeEnsino = InstituicaodeEnsino.query.filter_by(email=email).first()
+        if instituicaodeEnsino and check_password_hash(instituicaodeEnsino.senha, senha):
+            session['instituicaodeEnsino_id'] = instituicaodeEnsino.id
             return redirect(url_for('home'))  # Redireciona o navegador para acessar a rota /home
         else:
             return 'Credenciais inválidas!', 401
@@ -80,10 +80,10 @@ def login():
 
 @app.route('/home')
 def home():
-    faculdade = db.session.get(Faculdade, session.get('faculdade_id'))
-    if not faculdade:
+    instituicaodeEnsino = db.session.get(InstituicaodeEnsino, session.get('instituicaodeEnsino_id'))
+    if not instituicaodeEnsino:
         return redirect(url_for('login'))
-    return render_template('home.html', faculdade=faculdade)  # Renderiza o HTML com o chefe
+    return render_template('home.html', instituicaodeEnsino=instituicaodeEnsino)  # Renderiza o HTML com o chefe
 
 @app.route("/instituicaoEnsino")
 def instituicaoEnsino():
@@ -114,7 +114,7 @@ def instituicaoEnsino():
 
 @app.route('/logout')
 def logout():
-    session.pop('faculdade_id', None)
+    session.pop('instituicaodeEnsino_id', None)
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
