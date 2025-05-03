576cef6ce6fe14a35e98c1b53cdf979788ab2697
Amanda Camily <guilhermeplays03@gmail.com>
2025-05-03 17:46:10 -0300

    criando tabela chefe

--- app.py
+++ app.py
@@ -37,6 +37,16 @@ class Aluno(db.Model):
     formação = db.Column(db.String(255))
     periodo = db.Column(db.Integer)
 
+class Chefe(db.Model):
+    __tablename__ = 'chefe'
+
+    id_chefe = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100), nullable=False)
+    cargo = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True)  # Pode ser nulo, mas deve ser único se fornecido
+    senha = db.Column(db.String(255), nullable=False)
+    nome_empresa = db.Column(db.String(100))
+
 with app.app_context():
     db.create_all()  # mantido caso quiser criar outras tabelas
 
