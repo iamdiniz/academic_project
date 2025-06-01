a4063d715ea69616013d86ba25ae79fb633de61d
diniz <guilhermeplays03@gmail.com>
2025-06-01 19:30:41 -0300

    railway complete

--- app.py
+++ app.py
@@ -1393,4 +1393,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')  # host para expor o servidor para fora do container
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
